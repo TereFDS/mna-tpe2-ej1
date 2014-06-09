80a415ea1d1dc805dfaf9ed8e2419bd42c7169d5
Teresa F <tere.fds@hotmail.com>
2014-06-09 00:13:50 -0300

    cambios

--- src/partitureMaker.m
+++ src/partitureMaker.m
@@ -1,6 +1,6 @@
 function [tone partiture]= partitureMaker(fm,bitsQty,fileName)
-    
+    pitches=["C","C#","D","Eb","E","F","F#","G","Ab","A","Bb","B"];
     [audio fs bps]=wavread(fileName);
-    tone=max(fft(audio));
+    tone=max(ifft(audio));
     
 endfunction    
