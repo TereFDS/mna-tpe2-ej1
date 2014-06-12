0045f1426729f75827950c2a71a229a96e6fe2c3
Tomas <tomymehdi@gmail.com>
2014-06-12 01:39:37 -0300

    cooleytukey not working

--- src/cooleyTukeyFFT.m
+++ src/cooleyTukeyFFT.m
@@ -1,14 +1,16 @@
-function X=cooleyTukeyFFT(x,N,s)
-    X=zeros(N);
+function X = cooleyTukeyFFT(x,N,s)
+    X = zeros(1,N);
     if(N==1)
         X=x(1);
         return 
-    end
-        X(1:(N/2))=cooleyTukeyFFT(x,N/2,2*s);
-        X((N/2+1):N)=cooleyTukeyFFT(x(s:end),N/2,2*s);
+    else
+        X(1,1:(N/2)) = cooleyTukeyFFT(x,N/2,2*s);
+        [r c] = size(x);
+        X(1,((N/2)+1):N) = cooleyTukeyFFT(x(1,1:s:c),N/2,2*s);
         for k=1:N/2
-           t=x(k); 
-           X(k)=t+exp(-2*pi*i/N)*X(k+N/2);
-           X(k+N/2)=t+exp(-2*pi*i/N)*X(k+N/2); 
-        end    
-end    
+           t = x(k); 
+           X(k) = t + exp(-2*pi*1i*k/N) * X(k+(N/2));
+           X(k+N/2) = t - exp(-2*pi*1i*k/N) * X(k+(N/2)); 
+        end   
+    end
+end
