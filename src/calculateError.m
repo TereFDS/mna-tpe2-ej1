9fa90fb81d5dc1bf07d3264f4a7e3677584f7245
Tomas <tomymehdi@gmail.com>
2014-06-19 00:02:23 -0300

    error calculator

--- src/calculateError.m
+++ src/calculateError.m
@@ -1,6 +1,6 @@
 function [error] = calculateError(realWavFile, generatedWavFile)
-    tfqR = toneFreqs(50,1,realWavFile);
-    tfqG = toneFreqs(50,1,generatedWavFile);
+    tfqR = toneFreqs(50,16,realWavFile);
+    tfqG = toneFreqs(50,16,generatedWavFile);
     diff = abs(tfqR-tfqG).^2;
     squareError = sum(diff(:))/numel(X)
 end
